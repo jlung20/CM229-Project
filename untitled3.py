99c6f9480709e0315b5b28b9b5e08ae17968204e
Jeff Lung <jefflung248@yahoo.com>
2019-06-15 22:26:00 -0700

    Improvement to plotting program

--- untitled3.py
+++ untitled3.py
@@ -22,6 +22,8 @@ def calc_geno(haplo):
         return 0
 
 with open('complete20.vcf', 'r') as og, open('missing20.vcf', 'r') as missing, open('test.vcf', 'r') as imputed:
+    accs = np.load('accs.npy')
+    maf_dict = {}
     for ii in range(10):
         imputed.readline()
     for jj in range(253):
@@ -37,7 +39,6 @@ with open('complete20.vcf', 'r') as og, open('missing20.vcf', 'r') as missing, o
         ol = og.readline()
         imp = imputed.readline()
         if mline[9] == '.|.':
-            print(i)
             oline = ol.split()
             imline = imp.split()
             correct = 0
@@ -46,8 +47,10 @@ with open('complete20.vcf', 'r') as og, open('missing20.vcf', 'r') as missing, o
             for ii in range(9, 2013):
                 minor_allele_sum += calc_geno(oline[ii])
             tmaf = minor_allele_sum/4000.0
+            maf_dict[i - 1] = tmaf
             if tmaf > 0.5:
                 maf.append(1 - tmaf)
+                maf_dict[i - 1] = 1 - tmaf
             else:
                 maf.append(tmaf)
             for offset in range(500):
@@ -64,6 +67,14 @@ with open('complete20.vcf', 'r') as og, open('missing20.vcf', 'r') as missing, o
     print(maf)
     print(results)
     print(results_1)
-    plt.plot(maf, results, 'o')
+    lstm_res = []
+    lmaf = []
+    for tp in accs:
+        lstm_res.append(tp[1])
+        lmaf.append(maf_dict[tp[0]])
+    plt.plot(maf, results, 'o', label="BEAGLE")
+    plt.plot(lmaf, lstm_res, 'ro', label="BiLSTM")
     plt.xlabel('Minor Allele Frequency')
     plt.ylabel('Accuracy')
+    plt.legend(loc='best')
+    plt.savefig('beagle_blstm.pdf')
